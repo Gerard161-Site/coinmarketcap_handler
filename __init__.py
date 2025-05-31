735a2eb5d82657ef78da2dca91ca890fa37e51c9
Gerard Kavanagh <gerard161@gmail.com>
2025-05-31 22:45:46 +0100

    New code for coinmarketcap handler

--- __init__.py
+++ __init__.py
@@ -1,3 +1,7 @@
+import logging
+logging.basicConfig(level=logging.DEBUG)
+logger = logging.getLogger(__name__)
+logger.debug("Loading CoinMarketCap handler")
 
 from .__about__ import __version__ as version, __description__ as description
 from .connection_args import connection_args, connection_args_example
@@ -5,17 +9,18 @@ from .connection_args import connection_args, connection_args_example
 try:
     from .coinmarketcap_handler import CoinMarketCapHandler as Handler
     import_error = None
+    logger.debug("Successfully imported CoinMarketCapHandler")
 except Exception as e:
     Handler = None
     import_error = e
+    logger.error(f"Failed to import CoinMarketCapHandler: {e}")
 
 title = 'CoinMarketCap'
 name = 'coinmarketcap'
-type = HANDLER_TYPE.DATA
+type = 'data'  # Changed from HANDLER_TYPE.DATA
 icon_path = 'icon.svg'
 
 __all__ = [
     'Handler', 'version', 'name', 'type', 'title', 'description',
     'connection_args', 'connection_args_example', 'import_error', 'icon_path'
 ]
-
